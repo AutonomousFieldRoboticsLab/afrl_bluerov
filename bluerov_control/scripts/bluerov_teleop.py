3f334ab1d85154c7ce2ca30784e78ad4b308ca68
Bharat Joshi <robotics.bharat@gmail.com>
2021-10-12 11:36:51 -0400

    joystick updates

--- bluerov_control/scripts/bluerov_teleop.py
+++ bluerov_control/scripts/bluerov_teleop.py
@@ -8,21 +8,16 @@
 # https://github.com/mavlink/mavros/tree/master/LICENSE.md
 
 from __future__ import print_function
+from os import stat
 
-import argparse
-import sys
 
 import rospy
-from geometry_msgs.msg import (Point, PoseStamped, Quaternion, TwistStamped,
-                               Vector3)
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64, Header
-from tf.transformations import quaternion_from_euler
 
-import mavros
-from mavros import command
-from mavros import setpoint as SP
 from mavros_msgs.msg import OverrideRCIn
+from mavros_msgs.srv import CommandBool
+
+import numpy as np
 
 
 def arduino_map(x, inmin, inmax, outmin, outmax):
@@ -32,6 +27,13 @@ def arduino_map(x, inmin, inmax, outmin, outmax):
 def load_map(m, n):
     for k, v in m.items():
         m[k] = rospy.get_param(n + k, v)
+
+
+def get_defualt_rc():
+    rc = OverrideRCIn()
+    for i in range(len(rc.channels)):
+        rc.channels[i] = 1500
+    return rc
 
 
 class RCChan(object):
@@ -56,30 +58,38 @@ class RCChan(object):
 
 
 def arm(state):
+    rospy.wait_for_service('/mavros/cmd/arming')
     try:
-        command.arming(value=state)
-    except rospy.ServiceException as ex:
-        fault(ex)
+        armService = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+        armService(state)
+    except rospy.ServiceException as e:
+        rospy.loginfo('Service arm call failed: %s' % e)
 
-    if not ret.success:
-        rospy.loginfo("Request failed.")
-    else:
-        rospy.loginfo("Request success.")
+
+def construct_rc_message(channel_values):
+    rc_message = get_defualt_rc()
+
+    for (index, value) in enumerate(channel_values):
+        rc_message.channels[index] = value
+
+    return rc_message
 
 
 class JoyStick(object):
-    def __init__(self, name):
+    def __init__(self):
 
         # Joystick button map
         self.axes_map = {'forward': 3,
                          'lateral': 4,
                          'yaw': 0,
-                         'throttle': 1
+                         'throttle': 1,
+                         'lights': 2
                          }
         self.axes_scale = {'forward': 1.0,
                            'lateral': 1.0,
                            'yaw': 1.0,
-                           'throttle': 1.0
+                           'throttle': 1.0,
+                           'lights': 1.0
                            }
         self.rc_channels = {
             'forward': RCChan('forward', 0),
@@ -88,7 +98,22 @@ class JoyStick(object):
             'throttle': RCChan('throttle', 2)
         }
 
+        self.x_factor = [-1.0, -1.0, 1.0, 1.0, 0.0, 0.0]
+        self.y_factor = [-1.0, 1.0, -1.0, 1.0, 0.0, 0.0]
+        self.z_factor = [0.0, 0.0, 0.0, 0.0, -1.0, -1.0]
+
+        self.roll_factor = [0.0, 0.0, 0.0, 0.0, 1.0, -1.0]
+        self.pitch_factor = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.yaw_factor = [-1.0, 1.0, 1.0, -1.0, 0.0, 0.0]
+
+        self.directions = [-1.0, -1.0, -1.0, 1.0, 1.0, 1.0]
+
         self.button_map = {'arm': 0, 'disarm': 1}
+
+        self.forward_minimum_pwms = [20, 20, 20, 20, 20, 20, 20]
+        self.backward_minimum_pwms = [-20, -20, -20, -20, -20, -20]
+
+        self.max_motor_speed = 150
 
         rospy.loginfo('RC Override Control')
 
@@ -96,26 +121,47 @@ class JoyStick(object):
         print(self.axes_map)
         load_map(self.axes_scale, '~axes_scale/')
         print(self.axes_scale)
+        load_map(self.button_map, '~button_map/')
+        print(self.button_map)
+
+        for _, rc_chan in self.rc_channels.items():
+            rc_chan.load_param()
+
+        self.defualt_rc_value = 1500.0
+        self.max_pwm = 2000.0
+        self.min_pwm = 1000.0
+
+        self.light_value = self.min_pwm
+        self.light_steps = 4
+        self.light_button = 0.0
 
         self.joy_sub = rospy.Subscriber('joy', Joy, self.joy_cb)
         self.override_pub = rospy.Publisher(
             '/mavros/rc/override', OverrideRCIn, queue_size=10)
-
-        self.rc_command = OverrideRCIn()
-
-        for name, rc_chan in self.rc_channels.items():
-            rc_chan.load_param()
-
-        self.defualt_rc_value = 1500
-
         # for name, rc_chan in self.rc_channels.items():
         #     print('Name: {} '.format(name), rc_chan)
+
+    def convert_motor_intensities_to_pwms(self, intensities):
+        offset_motor_speeds = [1500, 1500, 1500, 1500, 1500, 1500]
+
+        for i in range(len(offset_motor_speeds)):
+            offset = 0
+
+            if intensities[i] < 0:
+                offset = self.backward_minimum_pwms[i]
+            elif intensities[i] > 0:
+                offset = self.forward_minimum_pwms[i]
+
+            offset_motor_speeds[i] = offset_motor_speeds[i] + \
+                (intensities[i] * self.max_motor_speed) + offset
+
+        return offset_motor_speeds
 
     def get_axis(self, joy_msg, n):
         return joy_msg.axes[self.axes_map[n]] * self.axes_scale[n]
 
     def get_buttons(self, joy_msg, n):
-        return j.buttons[self.button_map[n]]
+        return joy_msg.buttons[self.button_map[n]]
 
     def set_chan(self, name, value):
         ch = self.rc_channels[name]
@@ -129,25 +175,77 @@ class JoyStick(object):
         yaw = self.get_axis(joy_msg, 'yaw')
         throttle = self.get_axis(joy_msg, 'throttle')
 
-        # rospy.loginfo("RPYT: %f, %f, %f, %f", forward, lateral, yaw, throttle)
+        thurst = [forward, lateral, throttle, 0.0, 0.0, yaw]
+        motor_intensities = self.convert_thrust_vector_to_motor_intensities(
+            thurst)
 
-        self.rc_command = OverrideRCIn()
+        motor_intensities = [intensity * direction for intensity,
+                             direction in zip(motor_intensities, self.directions)]
 
-        for i in range(len(self.rc_command.channels)):
-            self.rc_command.channels[i] = self.defualt_rc_value
+        motor_pwms = self.convert_motor_intensities_to_pwms(motor_intensities)
 
-        self.set_chan('forward', forward)
-        self.set_chan('lateral', lateral)
-        self.set_chan('yaw', yaw)
-        self.set_chan('throttle', throttle)
+        for speed in motor_pwms:
+            assert((abs(speed) - 1500) <= 400)
 
-        self.override_pub.publish(self.rc_command)
+        rc_command = construct_rc_message(motor_pwms)
+
+        rc_command.channels = [int(x) for x in rc_command.channels]
+
+        # Handle lights
+
+        light = self.get_axis(joy_msg, 'lights')
+        if(light != self.light_button):
+            self.light_button = light
+            if light != 0.0:
+                self.get_light_intensity(light)
+
+        self.light_value = np.clip(
+            self.light_value, self.min_pwm, self.max_pwm)
+
+        #print('Light: {}'.format(self.light_value))
+
+        rc_command.channels[6] = int(self.light_value)
+        self.override_pub.publish(rc_command)
+
+        if self.get_buttons(joy_msg, 'arm'):
+            arm(True)
+        elif self.get_buttons(joy_msg, 'disarm'):
+            arm(False)
+
+    def convert_thrust_vector_to_motor_intensities(self, thrust_vector):
+
+        # thrust vector is: x,y,z,roll,pitch,yaw
+
+        motor_intensities = [0.0] * 6
+
+        max_intensity = 1.0
+
+        # x,y,yaw intensities
+        for i in range(len(motor_intensities)):
+            motor_intensities[i] = thrust_vector[0] * self.x_factor[i] + \
+                thrust_vector[1] * self.y_factor[i] + \
+                thrust_vector[2] * self.z_factor[i] + \
+                thrust_vector[3] * self.roll_factor[i] + \
+                thrust_vector[4] * self.pitch_factor[i] + \
+                thrust_vector[5] * self.yaw_factor[i]
+
+        if abs(motor_intensities[i]) > max_intensity:
+            max_intensity = abs(motor_intensities[i])
+
+        normalized_intensities = [
+            val / max_intensity for val in motor_intensities]
+
+        return normalized_intensities
+
+    def get_light_intensity(self, joy_input):
+        step = (self.max_pwm - self.min_pwm)/self.light_steps
+        self.light_value = self.light_value + joy_input * step
 
 
 if __name__ == '__main__':
     rospy.init_node("bluerov_teleop")
     # mavros.set_namespace(args.mavros_ns)
 
-    jostick = JoyStick('joystick')
+    jostick = JoyStick()
     while not rospy.is_shutdown():
         rospy.spin()
--- bluerov_control/scripts/test_motors.py
+++ bluerov_control/scripts/test_motors.py
@@ -5,8 +5,16 @@ import rospy
 
 default_pwm = 1500
 
+yaw_factor = [0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0]
+x_factor = [0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+y_factor = [0.0,  0.0, 0.0, 0.0, -1.0, 0.0, -1.0, 0.0]
 
-def get_defualt_RC():
+roll_factor = [1.0, 0.0, -1.0, -1.0, 0.0, -1.0, 0.0, 0.0]
+pitch_factor = [-1.0, 0.0, -1.0, 1.0, 0.0, -1.0, 0.0, 0.0]
+z_factor = [1.0, 0.0, -1.0, 1.0, 0.0, 1.0, 0.0, 0.0]
+
+
+def get_defualt_rc():
     rc = OverrideRCIn()
     for i in range(len(rc.channels)):
         rc.channels[i] = default_pwm
@@ -18,12 +26,20 @@ if __name__ == '__main__':
     rospy.init_node('test_motors')
     pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=10)
 
-    rc = get_defualt_RC()
+    rc = get_defualt_rc()
 
-    rospy.loginfo("Publishing 1200 to Channel 0")
-    rc.channels[0] = 1200
-    pub.publish(rc)
+    rospy.loginfo("Publishing 1350 to Channel 0")
+    rc.channels[0] = 1450
 
-    rospy.sleep(30)
-    rc.channels[0] = default_pwm
-    pub.publish(rc)
+    print(rc)
+
+    # rospy.sleep(30)
+    # rc.channels[0] = default_pwm
+    # pub.publish(rc)
+
+    rate = rospy.Rate(10)  # 10hz
+
+    while not rospy.is_shutdown():
+        print('Publishing RC: {}'.format(rc))
+        pub.publish(rc)
+        rate.sleep()

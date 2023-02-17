44438f27db6bbca5fcbdf07fee51acedb4122118
afrl-uofsc <afrl.uofsc@gmail.com>
2023-02-17 11:07:50 -0500

    modified screen to log; added tag detector

--- bluerov_bringup/scripts/bluerov_bringup.sh
+++ bluerov_bringup/scripts/bluerov_bringup.sh
@@ -1,5 +1,5 @@
-screen -dm -S camera sh /home/afrl/bluerov_ws/src/afrl_bluerov/bluerov_bringup/scripts/camera.sh
+screen -L -Logfile "/home/afrl/logs/camera_$(date +"%Y_%m_%d_%I_%M_%p").log" -dm -S camera sh /home/afrl/bluerov_ws/src/afrl_bluerov/bluerov_bringup/scripts/camera.sh
 sleep 5
-screen -dm -S mavros sh /home/afrl/bluerov_ws/src/afrl_bluerov/bluerov_bringup/scripts/mavros.sh
+screen -L -Logfile "/home/afrl/logs/mavros_$(date +"%Y_%m_%d_%I_%M_%p").log" -dm -S mavros sh /home/afrl/bluerov_ws/src/afrl_bluerov/bluerov_bringup/scripts/mavros.sh
 sleep 5
-screen -dm -S record sh /home/afrl/bluerov_ws/src/afrl_bluerov/bluerov_bringup/scripts/bag_recorder.sh
+screen -L -Logfile "/home/afrl/logs/bagrecorde_$(date +"%Y_%m_%d_%I_%M_%p").log" -dm -S record sh /home/afrl/bluerov_ws/src/afrl_bluerov/bluerov_bringup/scripts/bag_recorder.sh

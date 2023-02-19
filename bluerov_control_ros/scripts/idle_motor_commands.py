16c40a39f2d5cfcf2937e3d67073bb8b8b626468
Bharat Joshi <robotics.bharat@gmail.com>
2023-02-19 13:07:21 -0500

    arming

--- bluerov_control_ros/scripts/idle_motor_commands.py
+++ bluerov_control_ros/scripts/idle_motor_commands.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from mavros_msgs.msg import OverrideRCIn
+from mavros_msgs.srv import CommandBool
 import rospy
+
 
 def get_defualt_rc():
     rc = OverrideRCIn()
@@ -10,11 +12,21 @@ def get_defualt_rc():
     return rc
 
 
-if __name__=="__main__":
+def arm(state):
+    rospy.wait_for_service("/mavros/cmd/arming")
+    try:
+        armService = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
+        armService(state)
+    except rospy.ServiceException as e:
+        rospy.loginfo("Service arm call failed: %s" % e)
+
+
+if __name__ == "__main__":
     rospy.init_node("idle_motor_commands")
-    pub = rospy.Publisher("/mavros/rc/override", OverrideRCIn, queue_size=1)
+    pub = rospy.Publisher("/mavros/rc/override", OverrideRCIn, queue_size=10)
     rate = rospy.Rate(10)
+
+    arm(True)
     while not rospy.is_shutdown():
         pub.publish(get_defualt_rc())
         rate.sleep()
-    
